c86b8d8f79916a8906dd29591bb5d36525d57984
sergiolfq <63725987+sergiolf@users.noreply.github.com>
2021-02-13 22:35:11 +0100

    initial components for routes examples

---
+++ resources/js/components/AnualReport.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Anual Report</div>
+
+                    <div class="card-body">
+                        This is your anual report
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/MainMenu.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>MainMenu</h1>
+        <router-link :to="{name: 'anual'}">Anual</router-link>
+        <router-link :to="{name: 'monthly'}">Monthly</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import VueRouter from 'vue-router';
+    import AnualReport from './AnualReport.vue';
+    import MonthlyReport from './MonthlyReport.vue';
+
+    export default {
+        router: new VueRouter({
+            routes: [
+                {
+                    path: '/anual',
+                    name: 'anual',
+                    component: AnualReport
+                },
+                {
+                    path: "/",
+                    redirect : { name: 'anual'}
+                },
+                {
+                    path: "/monthly",
+                    name: 'monthly',
+                    component: MonthlyReport
+                }
+            ]
+        }),
+        mounted: function(){
+            console.log("mounted");
+        }
+    }
+</script>
+
+<style scoped>
+    a {
+        border: solid 1px black;
+        padding: 5px;
+        margin-bottom: 4px;
+    }
+    .router-link-active{
+        font-weight: bold;
+        border-bottom: none;
+    }
+</style>
